f26a6a91765425b448e47b812d4cda309ae11afd
Brad Touesnard <brad@touesnard.com>
2013-09-19 16:35:08 -0300

    Cleanup addons screen.

--- view/addons.php
+++ view/addons.php
@@ -12,17 +12,21 @@
 		$details_url = self_admin_url( 'plugin-install.php?tab=plugin-information&amp;plugin=' . $slug . '&amp;TB_iframe=true&amp;width=600&amp;height=550' );
 		?>
 
-		<article class="addon">
-			<h1><?php echo $addon['title']; ?></h1>
-			<a class="thickbox" href="<?php echo $details_url; ?>"><?php _e( 'Details', 'amazon-web-services' ); ?></a> | 
-			<?php
-			if ( file_exists( WP_PLUGIN_DIR . '/' . $addon['path'] ) ) {
-				echo '<span>' . _x( 'Installed', 'amazon-web-services' ) . '</span>';
-			}
-			else {
-				echo '<a class="install-now" href="' . $this->get_plugin_install_url( $slug ) . '">' . __( 'Install Now', 'amazon-web-services' ) . '</a>';
-			}
-			?>
+		<article class="addon as3cf">
+			<div class="info">
+				<h1><?php echo $addon['title']; ?></h1>
+				<ul class="actions">
+					<li><a class="thickbox" href="<?php echo $details_url; ?>"><?php _e( 'View Details', 'amazon-web-services' ); ?></a></li>
+					<?php
+					if ( file_exists( WP_PLUGIN_DIR . '/' . $addon['path'] ) ) {
+						echo '<li><span>' . _x( 'Already Installed', 'amazon-web-services' ) . '</span></li>';
+					}
+					else {
+						echo '<li><a class="install-now" href="' . $this->get_plugin_install_url( $slug ) . '">' . __( 'Install Now', 'amazon-web-services' ) . '</a></li>';
+					}
+					?>
+				</ul>
+			</div>
 		</article>
 
 		<?php
